8564d3d7f12dfb163b0186ec1cd965bce4e19b70
Michał Maksimowicz <m.m.maksimowicz@gmail.com>
2019-12-10 22:52:33 +0100

    Final commit

--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,4 @@
+//Enum for specifing dateTime interval
 const DateIntervals ={
     SECONDS : "seconds",
     MINUTES : "minutes",
@@ -5,11 +6,52 @@ const DateIntervals ={
     DAYS : "days"
 }
 
+//Method to verify if given value is DateInterval
+function checkIfDateInterval(intervalToCheck){
+   var result = false;
+   
+   for(var intervalValue in DateIntervals)
+   {
+       if(DateIntervals[intervalValue] == intervalToCheck)
+       {
+           return true; //value found in dictionary
+       }
+   }
 
+   return result;   
+}
 
-function getDays(miliseconds)
+//Converts miliseconds to given time interval
+function getTimeIntervalFromMiliseconds(miliseconds, interval = DateIntervals.SECONDS)
 {
+    //Check if parameters are proper
+    if(!checkIfDateInterval(interval))
+    {
+        throw Error("Date interval is invalid!");     
+    }
 
+    if(isNaN(miliseconds))
+    {
+        throw Error("Passed parameter is not a number!");
+    }
+
+    switch(interval)
+    {
+        case DateIntervals.SECONDS:
+            return (miliseconds/1000);
+            break;
+        case DateIntervals.MINUTES:
+            return (miliseconds/60000); //1000*60
+            break;
+        case DateIntervals.HOURS:
+            return (miliseconds/3600000);//1000*60*60
+            break;
+        case DateIntervals.DAYS:
+            return (miliseconds/86400000); //1000*60*60*24
+            break;
+        default:
+            throw Error("Wrong use of function.");
+    }
 }
 
 
@@ -19,8 +61,6 @@ function startDaysChecking(){
     document.getElementById("main-div").style.display = "none";
 }
 
-
-
 function checkDays(){
     var dateInnerString = document.getElementById("date-input").value;
     
@@ -28,17 +68,18 @@ function checkDays(){
     var dateNow = new Date();
 
     var diff = dateNow - dateInput;
-    console.log(typeof(diif));
+    
+    
+    var daysAlive = getTimeIntervalFromMiliseconds(diff,DateIntervals.DAYS);
+    var resultString = Math.trunc(daysAlive) + " days!";
+    document.getElementById("result-p").innerHTML = resultString;
+    document.getElementById("result-box").style.display = "block";
+}
 
-    if(isNaN(diff))
-    {
-        console.log("NOT A NUMBER");
-    }
- //   TimeUnit
+function reset(){
+    document.getElementById("date-input").value = null;
 
-    //var nowDate = new Date();
-    //console.log("Date now: " + nowDate);
-
-    //console.log(nowDate - date);
-
+    document.getElementById("result-box").style.display = "none";
+    document.getElementById("date-div").style.display = "none";
+    document.getElementById("main-div").style.display = "flex";
 }
